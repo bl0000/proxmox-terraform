30435d69038c751b760c154e4bb3afec873ff23b
ben <ben@bl-lab.net>
2025-07-12 11:58:25 +0000

    Edit terraform.tf

--- src/terraform.tf
+++ src/terraform.tf
@@ -6,14 +6,7 @@ terraform {
     }
   }
 
-   backend "http" {
-    address = "http://gitlab.bl-lab.net/api/v4/projects/6/terraform/state/tfstate"
-    lock_address = "http://gitlab.bl-lab.net/api/v4/projects/6/terraform/state/tfstate"
-    unlock_address = "http://gitlab.bl-lab.net/api/v4/projects/6/terraform/state/tfstate"
-    lock_method = "POST"
-    unlock_method = "DELETE"
-    retry_wait_min = 5
-   }
+   backend "http" {}
 }
 
 provider "proxmox" {}
