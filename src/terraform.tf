bf6d18fec1471e8c5cb97c6912ee5853f0b32379
ben <82478568+bl0000@users.noreply.github.com>
2025-07-12 11:51:52 +0100

    Configured Gitlab HTTP backend

--- src/terraform.tf
+++ src/terraform.tf
@@ -5,6 +5,11 @@ terraform {
       version = ">= 0.79.0"
     }
   }
+
+  backend "http" {
+    address = "http://gitlab.bl-lab.net/api/v4/projects/6/terraform/state/proxmox_state"
+    lock_address = "http://gitlab.bl-lab.net/api/v4/projects/6/terraform/state/proxmox_state"
+    unlock_address = "http://gitlab.bl-lab.net/api/v4/projects/6/terraform/state/proxmox_state"
 }
 
 provider "proxmox" {}
