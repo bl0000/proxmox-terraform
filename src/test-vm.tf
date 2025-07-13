c8a22c3bc7fa2227b701cf818682be6043efe0ee
ben <82478568+bl0000@users.noreply.github.com>
2025-07-13 11:39:46 +0100

    Added additional VM for testing

--- src/test-vm.tf
+++ src/test-vm.tf
@@ -4,3 +4,16 @@ module "test04" {
   ip_address = "10.0.0.5/24"
   gateway = "10.0.0.2"
 }
+
+module "test05" {
+  source = "./modules/virtual-machine"
+  vm_name = "test05"
+
+  cores = 4
+  memory = 4096
+
+  disk_size = 25
+
+  ip_address = "10.0.0.6/24"
+  gateway = "10.0.0.2"
+}
