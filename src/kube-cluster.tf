baad36f4bba72db9a02e7b55e6a80dc61a2ea935
bl0000 <82478568+bl0000@users.noreply.github.com>
2025-12-02 22:55:27 +0000

    Recreated kube-cluster.tf

---
+++ src/kube-cluster.tf
@@ -0,0 +1,57 @@
+module "kube01" {
+  source  = "./modules/virtual-machine"
+  vm_name = "kube01"
+
+  cores  = 2
+  memory = 4096
+
+  disks = [
+    {
+      size = 25
+    }
+  ]
+
+  vlan_tag = 1008
+
+  ip_address = "10.15.1.210/28"
+  gateway    = "10.15.1.209"
+}
+
+module "kube02" {
+  source  = "./modules/virtual-machine"
+  vm_name = "kube02"
+
+  cores  = 2
+  memory = 4096
+
+  disks = [
+    {
+      size = 25
+    }
+  ]
+
+  vlan_tag = 1008
+
+  ip_address = "10.15.1.211/28"
+  gateway    = "10.15.1.209"
+}
+
+module "kube03" {
+  source  = "./modules/virtual-machine"
+  vm_name = "kube03"
+
+  cores  = 2
+  memory = 4096
+
+  disks = [
+    {
+      size = 25
+    }
+  ]
+
+  vlan_tag = 1008
+
+  ip_address = "10.15.1.212/28"
+  gateway    = "10.15.1.209"
+}
+
