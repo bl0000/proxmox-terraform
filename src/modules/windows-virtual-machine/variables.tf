896f8fbd3a20df1b3fcddee10372e8a529c8a936
ben <82478568+bl0000@users.noreply.github.com>
2026-02-14 18:33:45 +0000

    Created Windows VM module based on Linux VM module

---
+++ src/modules/windows-virtual-machine/variables.tf
@@ -0,0 +1,45 @@
+variable "vm_name" {
+  type = string
+}
+
+variable "cores" {
+  type    = number
+  default = 4
+}
+
+variable "memory" {
+  type    = number
+  default = 4096
+
+}
+
+variable "disks" {
+  description = "List of disks"
+  type = list(object({
+    size         = number
+    datastore_id = optional(string)
+    backup       = optional(bool, true)
+    cache        = optional(string, "writeback")
+    discard      = optional(string, "on")
+    iothread     = optional(bool, true)
+    replicate    = optional(bool, true)
+  }))
+  default = []
+}
+
+variable "vlan_tag" {
+  type = number
+}
+
+variable "dns_servers" {
+  type    = list(string)
+  default = ["1.1.1.1"]
+}
+
+variable "ip_address" {
+  type = string
+}
+
+variable "gateway" {
+  type = string
+}
