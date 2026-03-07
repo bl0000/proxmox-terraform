60573268f62db4a76d9d7ff548037b03a0aa8ec9
ben <ben@bl-lab.net>
2026-03-07 18:09:10 +0000

    Created ai.tf

---
+++ src/ai.tf
@@ -0,0 +1,21 @@
+# VM for some Docker-based AI testing
+
+module "ai01" {
+  source  = "./modules/virtual-machine"
+  vm_name = "ai01"
+
+  cores  = 4
+  memory = 4196
+
+  disks = [
+    {
+      size = 25
+      datastore_id = "nvme-lvm"
+    }
+  ]
+
+  vlan_tag = 1009
+
+  ip_address = "10.15.1.226/28"
+  gateway    = "10.15.1.225"
+}
