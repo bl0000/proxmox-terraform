39efe490a698dd1e9179f12c79fb8c313195574a
ben <ben@bl-lab.net>
2026-05-30 18:02:11 +0000

    Added mc02

---
+++ src/mc.tf
@@ -0,0 +1,19 @@
+module "mc02" {
+  source  = "./modules/virtual-machine"
+  vm_name = "mc02"
+
+  cores  = 4
+  memory = 10240
+
+  disks = [
+    {
+      size = 100
+      datastore_id = "nvme-lvm"
+    }
+  ]
+
+  vlan_tag = 1004
+
+  ip_address = "10.15.1.147/28"
+  gateway    = "10.15.1.145"
+}
